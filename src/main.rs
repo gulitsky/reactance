25e8f0493ffb2b999f54c8271816d82ebfc82c81
Andrey Gulitsky <angulitsky@gmail.com>
2019-12-01 05:24:28 +0600

    Flushing changes

--- src/main.rs
+++ src/main.rs
@@ -22,22 +22,18 @@ use panic_reset as _;
 #[cfg(all(debug_assertions, feature = "panic-semihosting"))]
 use panic_semihosting as _;
 
-use nrf52840_hal::{clocks, prelude::*, target as device};
+use stm32f4xx_hal::{prelude::*, stm32 as device};
 
 use rtfm::app;
 
-#[app(device = nrf52840_hal::target, peripherals = true)]
+#[app(device = stm32f4xx_hal::stm32, peripherals = true)]
 const APP: () = {
     #[init]
     fn init(c: init::Context) {
         let _cp: cortex_m::Peripherals = c.core;
-        let dp: device::Peripherals = c.device;
+        let _dp: device::Peripherals = c.device;
 
-        let _clocks = dp
-            .CLOCK
-            .constrain()
-            .enable_ext_hfosc()
-            .set_lfclk_src_external(clocks::LfOscConfiguration::NoExternalNoBypass)
-            .start_lfclk();
+        let rcc: stm32f4xx_hal::rcc::Rcc = _dp.RCC.constrain();
+        let _clocks: stm32f4xx_hal::rcc::Clocks = rcc.cfgr.use_hse(168.mhz()).freeze();
     }
 };
