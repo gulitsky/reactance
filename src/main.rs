5fa78013cbe50bed06982c6b2606732bca48de35
Eugene Elpashev <eelpashev@gmail.com>
2021-04-10 03:27:59 +0600

    Flushing changes.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![no_main]
 #![no_std]
 
-use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use defmt_rtt as _;
+use panic_probe as _;
 use stm32g4::stm32g474 as pac;
 
 mod board;
@@ -13,15 +13,13 @@ const APP: () = {
 
     #[init]
     fn init(cx: init::Context) {
-        rtt_init_print!();
-
         let mut core = cx.core;
         // Enable the monotonic timer
         core.DCB.enable_trace();
         core.DWT.enable_cycle_counter();
 
         board::init();
-        rprintln!("Reactance");
+        defmt::info!("Reactance");
     }
 
     #[idle]
